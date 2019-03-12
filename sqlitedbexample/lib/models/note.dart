1976f3e5ccb5c8591a5db0c304881a39818a16a2
Nagarjuna <nagarjuna.v@apalya.com>
2019-03-12 17:05:20 +0530

    SqFLite Project example added

---
+++ sqlitedbexample/lib/models/note.dart
@@ -0,0 +1,65 @@
+class Note {
+  int _id;
+  String _title;
+  String _description;
+  String _date;
+  int _priority;
+
+  Note(this._title,this._date,this._priority,[this._description]);
+
+  Note.withId(this._id,this._date,this._title,this._priority,[this._description]);
+
+  int get   id=> _id;
+
+  String get title=> _title;
+
+  String get description=> _description;
+
+  String get date => _date;
+
+  int get priority=>_priority;
+
+  set title (String newTitle){
+    if (newTitle.length<=255) {
+      this._title=newTitle;
+    }
+  }
+    set description (String newdescription){
+    if (newdescription.length<=255) {
+      this._description=newdescription;
+    }
+  }
+
+   set priority (int newpriority){
+     if (newpriority>=1&&newpriority<=2) {
+      this._priority=newpriority; 
+     }
+  }
+
+   set date (String newDate){
+     this._date=newDate;
+   } 
+// Convert a Note object into a Map object
+   Map<String, dynamic> toMap() {
+
+		var map = Map<String, dynamic>();
+		if (id != null) {
+			map['id'] = _id;
+		}
+		map['title'] = _title;
+		map['description'] = _description;
+		map['priority'] = _priority;
+		map['date'] = _date;
+
+		return map;
+	}
+
+  // Extract a Note object from a Map object
+	Note.fromMapObject(Map<String, dynamic> map) {
+		this._id = map['id'];
+		this._title = map['title'];
+		this._description = map['description'];
+		this._priority = map['priority'];
+		this._date = map['date'];
+	}
+}
---
+++ sqlitedbexample/lib/screens/note_detail.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:sqlitedbexample/models/note.dart';
+import 'package:sqlitedbexample/utils/database_helper.dart';
+
+class NoteDetails extends StatefulWidget {
+  final String appBarTitle;
+  final Note note;
+  NoteDetails(this.note, this.appBarTitle);
+
+  @override
+  State<StatefulWidget> createState() {
+    return NoteDetailState(this.note, this.appBarTitle);
+  }
+}
+
+class NoteDetailState extends State<NoteDetails> {
+  DatabaseHelper helper = DatabaseHelper();
+  String appBarTitle;
+  Note note;
+  static var _proporties = ['High', 'Low'];
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptioncontroller = TextEditingController();
+  NoteDetailState(this.note, this.appBarTitle);
+  @override
+  Widget build(BuildContext context) {
+    TextStyle textStyle = Theme.of(context).textTheme.subhead;
+    titleController.text = note.title;
+    descriptioncontroller.text = note.description;
+    return WillPopScope(
+        onWillPop: () {
+          moveToLastScreen();
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(appBarTitle),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                moveToLastScreen();
+              },
+            ),
+          ),
+          body: Padding(
+            padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+            child: ListView(
+              children: <Widget>[
+                ListTile(
+                  title: DropdownButton(
+                      items: _proporties.map((String dropDownStringItem) {
+                        return DropdownMenuItem<String>(
+                          value: dropDownStringItem,
+                          child: Text(dropDownStringItem),
+                        );
+                      }).toList(),
+                      style: textStyle,
+                      value: getPriorityAsString(note.priority),
+                      onChanged: (valueSelectedByUser) {
+                        setState(() {
+                          debugPrint('User selected $valueSelectedByUser');
+                          updatePriorityAsInt(valueSelectedByUser);
+                        });
+                      }),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  child: TextField(
+                    controller: titleController,
+                    style: textStyle,
+                    onChanged: (value) {
+                      updateTitle();
+                    },
+                    decoration: InputDecoration(
+                        labelText: 'Title',
+                        labelStyle: textStyle,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0))),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  child: TextField(
+                    controller: descriptioncontroller,
+                    style: textStyle,
+                    onChanged: (value) {
+                      updateDescription();
+                    },
+                    decoration: InputDecoration(
+                        labelText: 'Description',
+                        labelStyle: textStyle,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0))),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: RaisedButton(
+                          color: Theme.of(context).primaryColorDark,
+                          textColor: Theme.of(context).primaryColorLight,
+                          child: Text(
+                            'Save',
+                            textScaleFactor: 1.5,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _save();
+                            });
+                          },
+                        ),
+                      ),
+                      Container(
+                        width: 5.0,
+                      ),
+                      Expanded(
+                        child: RaisedButton(
+                          color: Theme.of(context).primaryColorDark,
+                          textColor: Theme.of(context).primaryColorLight,
+                          child: Text(
+                            'Delete',
+                            textScaleFactor: 1.5,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _delete();
+                            });
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+
+  // Convert the String priority in the form of integer before saving it to Database
+  void updatePriorityAsInt(String value) {
+    switch (value) {
+      case 'High':
+        note.priority = 1;
+        break;
+      case 'Low':
+        note.priority = 2;
+        break;
+    }
+  }
+
+  // Convert int priority to String priority and display it to user in DropDown
+  String getPriorityAsString(int value) {
+    String priority;
+    switch (value) {
+      case 1:
+        priority = _proporties[0]; // 'High'
+        break;
+      case 2:
+        priority = _proporties[1]; // 'Low'
+        break;
+    }
+    return priority;
+  }
+
+  void updateTitle() {
+    note.title = titleController.text;
+  }
+
+  // Update the description of Note object
+  void updateDescription() {
+    note.description = descriptioncontroller.text;
+  }
+
+  void moveToLastScreen() {
+    Navigator.pop(context,true);
+  }
+
+  void _save() async {
+    moveToLastScreen();
+    note.date=DateFormat.yMMMd().format(DateTime.now());
+    int result;
+    if (note.id != null) {
+      result = await helper.updateNote(note);
+    } else {
+      result = await helper.insertNote(note);
+    }
+
+    if (result != 0) {
+      showAlertDialog('Status', 'Note Saved Successfully');
+    } else {
+      showAlertDialog('Status', 'Problem Saving Note');
+    }
+  }
+
+  void _delete() async{
+    moveToLastScreen();
+    if(note.id==null){
+      showAlertDialog('Status', 'No Note was deleted');
+      return;
+    }
+
+    int result =await helper.deleteNote(note.id);
+    if (result != 0) {
+      showAlertDialog('Status', 'Note Deleted Successfully');
+    } else {
+      showAlertDialog('Status', 'Error Occured while Deleting note');
+    }
+  }
+
+  void showAlertDialog(String title, String message) {
+    AlertDialog alertDialog = AlertDialog(
+      title: Text(title),
+      content: Text(message),
+    );
+    showDialog(context: context, builder: (_) => alertDialog);
+  }
+}
